f9d912032ba709f3b12381d4a0ccbb6efc383cdf
Andrew Buensalida <andrewcbuensalida@gmail.com>
2023-06-27 16:00:28 -0700

    get course with lessons of user

--- src/main/java/com/anhonestobserver/springbootneo4j/controllers/CourseEnrollmentController.java
+++ src/main/java/com/anhonestobserver/springbootneo4j/controllers/CourseEnrollmentController.java
@@ -1,5 +1,8 @@
 package com.anhonestobserver.springbootneo4j.controllers;
 
+import com.anhonestobserver.springbootneo4j.models.Course;
+import com.anhonestobserver.springbootneo4j.models.Lesson;
+import com.anhonestobserver.springbootneo4j.objects.CourseDTO;
 import com.anhonestobserver.springbootneo4j.objects.CourseEnrollmentDTO;
 import com.anhonestobserver.springbootneo4j.queryresults.CourseEnrollmentQueryResult;
 import com.anhonestobserver.springbootneo4j.requests.CourseEnrollmentRequest;
@@ -10,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/enrollments")
@@ -20,6 +25,19 @@ public class CourseEnrollmentController {
     public CourseEnrollmentController(CourseEnrollmentService courseEnrollmentService, LessonService lessonService) {
         this.courseEnrollmentService = courseEnrollmentService;
         this.lessonService = lessonService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<CourseDTO>> enrollments(Principal principal){
+        List<Course> courses = courseEnrollmentService.getAllEnrolledCoursesByUsername(principal.getName());
+        List<CourseDTO> responseCourses = courses.stream().map(
+                course -> {
+                    CourseDTO responseCourse = new CourseDTO(course.getIdentifier(), course.getTitle(), course.getTeacher());
+                    responseCourse.setLessons(lessonService.getAllLessonsByCourseIdentifier(course.getIdentifier()));
+                    return responseCourse;
+                }
+        ).collect(Collectors.toList());
+        return new ResponseEntity<>(responseCourses,HttpStatus.OK);
     }
 
     @PostMapping("/")
