f415d6a157ebedb8a296af501ae60c51c4ba8b1d
Andrew Buensalida <andrewcbuensalida@gmail.com>
2023-06-26 17:05:29 -0700

    add lessons when getting all courses

--- src/main/java/com/anhonestobserver/springbootneo4j/controllers/CourseController.java
+++ src/main/java/com/anhonestobserver/springbootneo4j/controllers/CourseController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/courses")
@@ -27,8 +28,14 @@ public class CourseController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<Course>> courseIndex(){
-        return new ResponseEntity<>(courseService.getAllCourses(), HttpStatus.OK);
+    public ResponseEntity<List<CourseDTO>> courseIndex(){
+        List<Course> courses = courseService.getAllCourses();
+        List<CourseDTO> responseCourses = courses.stream().map((course -> {
+            CourseDTO responseCourse = new CourseDTO(course.getIdentifier(),course.getTitle(),course.getTeacher());
+            responseCourse.setLessons(lessonService.getAllLessonsByCourseIdentifier(course.getIdentifier()));
+            return responseCourse;
+        })).collect(Collectors.toList());
+        return new ResponseEntity<>(responseCourses, HttpStatus.OK);
     }
 
     @GetMapping("/{identifier}")
